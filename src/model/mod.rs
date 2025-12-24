f1d40a158ad50168bda2cbc58b77d77fbb1254b4
siddshashi <siddshashi5@gmail.com>
2025-12-23 21:08:20 -0800

    creating model module

---
+++ src/model/mod.rs
@@ -0,0 +1,10 @@
+//this will include:
+//  layernorm
+//  single attention head
+//  multiple-head attention
+//  feedforward MLP
+//  dropout
+//  connecting everything with blocks, residual connections
+//  evaluating loss, cross-entropy
+//  backpropation with optimizer
+
